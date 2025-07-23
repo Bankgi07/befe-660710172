0b35e160e19b6267447658b7c9b479738b62f062
Thammatorn Likhitviwatkul <LIKHITVIWATKUL_T@SU.AC.TH>
2025-07-23 03:28:11 +0000

    backend assignment1

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello world")
+	var input string
+
+	fmt.Println("What your name")
+	fmt.Scan(&input)
+	fmt.Printf("Hello, %s!\n", input)
+}
